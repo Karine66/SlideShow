7eef8552c226288fb4e7691ab77f90d4e69fd636
Karine66 <shon@live.fr>
2021-03-04 18:21:18 +0100

    countDownTimer for photo bundle

--- app/src/main/java/com/karine/slideshow/MainActivity.kt
+++ app/src/main/java/com/karine/slideshow/MainActivity.kt
@@ -2,6 +2,7 @@ package com.karine.slideshow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
 import com.karine.slideshow.databinding.ActivityMainBinding
@@ -13,6 +14,7 @@ private lateinit var mainBinding: ActivityMainBinding
 val photoList = arrayOf(R.drawable.chat1, R.drawable.chat2, R.drawable.chat3, R.drawable.chien1,
 R.drawable.chien2, R.drawable.chien3)
 var currentPhotoIndex = 0
+    var countDownTimer:CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,19 +23,59 @@ var currentPhotoIndex = 0
         setContentView(view)
     }
 
+    override fun onResume() {
+        super.onResume()
+        startSlideShow()
+    }
+
     fun previousButtonTouched(button : View) {
+        stopSlideShow()
         currentPhotoIndex = currentPhotoIndex - 1
         if(currentPhotoIndex < 0) {
             currentPhotoIndex = photoList.size -1
         }
         mainBinding.imageView.setImageResource(photoList[currentPhotoIndex])
+        startSlideShow()
     }
 
     fun nextButtonTouched(button : View) {
-      currentPhotoIndex = currentPhotoIndex + 1
+        stopSlideShow()
+      showNextPhoto()
+        startSlideShow()
+    }
+
+    fun stopSlideShow() {
+        countDownTimer?.cancel()
+        countDownTimer = null
+    }
+
+    private fun startSlideShow() {
+        countDownTimer = object : CountDownTimer(3000,3000){
+            override fun onFinish() {
+               showNextPhoto()
+                startSlideShow()
+            }
+            override fun onTick(millisUntilFinished: Long) {}
+        }
+        countDownTimer?.start()
+    }
+
+    fun showNextPhoto() {
+        currentPhotoIndex = currentPhotoIndex + 1
         if(currentPhotoIndex >= photoList.size) {
             currentPhotoIndex = 0
         }
+        mainBinding.imageView.setImageResource(photoList[currentPhotoIndex])
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("currentPhotoIndex", currentPhotoIndex)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        currentPhotoIndex = savedInstanceState.getInt("currentPhotoIndex")
         mainBinding.imageView.setImageResource(photoList[currentPhotoIndex])
     }
 }
